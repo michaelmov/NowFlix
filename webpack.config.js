fe1dcfab2e42e070f098ad7b39a6df09ba5e4778
Michael Movsesov <michael.movsesov@gmail.com>
2017-01-17 21:38:21 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: "index.js",
+	output: {
+		path: __dirname + "/dist", 
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loaders: ["babel-loader"]
+			}
+		]
+	}
+}
