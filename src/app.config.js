969aa991f0ae7bf3a54b070ed35bc2f341c12c43
Michael Movsesov <michael.movsesov@gmail.com>
2017-04-14 16:25:19 -0700

    Disabled html5 mode for compatibility with github pages.

--- src/app.config.js
+++ src/app.config.js
@@ -5,7 +5,7 @@
 function routing($urlRouterProvider, $locationProvider) {
     'ngInject';
 
-    $locationProvider.html5Mode(true);
+    // $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/');
 }
 
