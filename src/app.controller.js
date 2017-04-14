50cd1f1da4c2fbbad8bd911aefc124351d744ed7
Michael Movsesov <michael.movsesov@gmail.com>
2017-04-14 16:25:58 -0700

    replaced hrefs with ui-sref for proper state routing.

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,27 +1,28 @@
 import angular from 'angular';
 
 export default class AppController {
-    constructor($location, $rootScope, $timeout) {
+    constructor($location, $state, $timeout) {
         'ngInject';
 
         this.$location = $location;
+        this.$state = $state;
         this.$timeout =  $timeout;
         this.navItems = [
             {
                 name: 'Now Playing',
-                url: '/',
+                url: 'nowPlayingMovies',
                 active: false,
                 pageTitle: 'Now Playing'
             },
             {
                 name: 'Upcoming',
-                url: '/upcoming',
+                url: 'upcomingMovies',
                 active: false,
                 pageTitle: 'Upcoming Movies'
             },
             {
                 name: 'Top Rated',
-                url: '/top',
+                url: 'topRatedMovies',
                 active: false,
                 pageTitle: 'Top Rated Movies'
             }
@@ -52,7 +53,7 @@ export default class AppController {
 
             // Set active state to true if url in "navItems" object matches the browser's url path.
             angular.forEach(this.navItems, (item) => {
-                if (item.url === this.$location.path()) {
+                if (item.url === this.$state.current.name) {
                     item.active = true;
                 }
             });
